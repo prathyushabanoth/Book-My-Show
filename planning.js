6eb0b1f348dac7af3339e05830fd93f226ed36ac
prathyushabanoth <prathyushabanoth4@gmail.com>
2022-01-06 16:57:37 +0530

    Project Setup JavaScript

---
+++ planning.js
@@ -0,0 +1,22 @@
+// Book my show planning
+
+// we need a reusable corousel
+//  -> Hero carousel
+//  -> Common carousel 
+//  -> cast and crew small carousel
+
+// Pages
+//  -> Home page
+//  -> movie page
+// -> categories page
+// -> Authentication page
+// -> Razorpay payment gateway
+
+//  Plan -> How are we building it?
+//  Layouts
+//  Routes
+//  pages
+//  components
+
+
+// HOC -> Higher Order Components
